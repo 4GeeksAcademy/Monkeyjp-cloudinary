6c60273caf5dee72dffbccc95407ebdfe3f2b145
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2025-01-23 10:07:23 +0000

    cloudinary

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,21 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import os
+
+#Cloudinary
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+cloudinary.config( 
+    cloud_name = os.getenv('CLOUD_NAME'),
+    api_key = os.getenv('CLOUDINARY_API_KEY'), 
+    api_secret = os.getenv('CLOUDINARY_API_SECRET'), # Click 'View API Keys' above to copy your API secret
+    secure=True
+)
+
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +35,14 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/img', methods=["POST"])
+def upload_image():
+    img = request.files["img"]
+    #print(img)
+    img_url = cloudinary.uploader.upload(img)
+    #print(img_url)
+
+
+    return jsonify({"img": img_url["url"]}), 200
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
@@ -6,21 +6,69 @@ import "../../styles/home.css";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const [file, setFile] = useState("")
+	const [fileUrl, setFileUrl] = useState("")
+
+	const handleImgChange = (e) => {
+		if (e.target.files.length) {
+			console.log(e.target.files);
+			setFile(e.target.files[0])
+		}
+		
+	}
+	const sendFile = async () => {
+		if (!file) {
+			alert("Image field is required")
+			return false
+		}
+		try {
+			const form = new FormData()
+			form.append("img", file)
+
+
+			//fetch to upload the img in the backend
+			const response = await fetch(`${process.env.BACKEND_URL}/api/img`, {
+				method: "POST",
+				body: form
+			})
+			console.log(response);
+			
+			const data = await response.json()
+			console.log(data);
+			setFileUrl(data.img)
+			
+		} catch (error) {
+			
+		}
+	}
+	console.log(fileUrl);
+	
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="row m-5 bg-secondary bg-opacity-10 p-2">
+			<div className="col-12 mb-3">
+				<h4 className="m-2">
+					Cloudinary
+				</h4>
+				<input 
+					type="file" 
+					className="form-control mb-2" 
+					accept="image/jpeg"
+					onChange={e => handleImgChange(e)}
+				/>
+				<button className="btn btn-primary" onClick={sendFile}>Send</button>
+
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+				{
+					fileUrl !== ""
+					?
+					<div className="col-4">
+						<img src={fileUrl} className="w-100 h-100"/>
+					</div>
+					:
+					null
+				}
+
 		</div>
 	);
 };
